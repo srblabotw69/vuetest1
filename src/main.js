12d7de6d51da943e930ac409c95941faeb730d08
srblabotw69 <mus56789@gmail.com>
2024-07-28 12:57:49 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
-// import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { createApp } from 'vue'
 import App from './App.vue'
-// import { Root } from "./Root"
 
 createApp(App).mount('#app')
